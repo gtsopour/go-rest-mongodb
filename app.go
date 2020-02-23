b498e9d42bc89f630812c9d2123a1b6e81a15e1c
George Tsopouridis <george.tsopouridis@sunrise.net>
2020-02-23 13:04:21 +0100

    Introduced Place controller

--- app.go
+++ app.go
@@ -1,32 +1,18 @@
 package main
 import (
-	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
+	"go-rest-mongodb/controllers"
 )
-func GetAllPlaces(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintln(w, "Not implemented")
-}
-func GetPlaceById(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintln(w, "Not implemented")
-}
-func CreatePlace(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintln(w, "Not implemented")
-}
-func UpdatePlace(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintln(w, "Not implemented")
-}
-func DeletePlace(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintln(w, "Not implemented")
-}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/places", GetAllPlaces).Methods("GET")
-	r.HandleFunc("/places/{id}", GetPlaceById).Methods("GET")
-	r.HandleFunc("/places", CreatePlace).Methods("POST")
-	r.HandleFunc("/places", UpdatePlace).Methods("PUT")
-	r.HandleFunc("/places", DeletePlace).Methods("DELETE")
+	r.HandleFunc("/places", controllers.GetAllPlaces).Methods("GET")
+	r.HandleFunc("/places/{id}", controllers.GetPlaceById).Methods("GET")
+	r.HandleFunc("/places", controllers.CreatePlace).Methods("POST")
+	r.HandleFunc("/places", controllers.UpdatePlace).Methods("PUT")
+	r.HandleFunc("/places", controllers.DeletePlace).Methods("DELETE")
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal(err)
 	}
---
+++ controllers/place.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+  "fmt"
+	//"encoding/json"
+	//"go-rest-mongodb/models"
+	"net/http"
+)
+
+var GetAllPlaces = func(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintln(w, "Not implemented")
+}
+
+var GetPlaceById = func(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintln(w, "Not implemented")
+}
+
+var CreatePlace = func(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintln(w, "Not implemented")
+}
+
+var UpdatePlace = func(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintln(w, "Not implemented")
+}
+
+var DeletePlace = func(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintln(w, "Not implemented")
+}
